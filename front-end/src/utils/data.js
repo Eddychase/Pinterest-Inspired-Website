0f0162f4a83a1f7b36511434bacd55ad8a409833
Eddy Chase <eddykanyz.ek@gmail.com>
2022-03-06 15:07:47 +0300

    Feed and Search Query

--- front-end/src/utils/data.js
+++ front-end/src/utils/data.js
@@ -76,3 +76,29 @@ export const feedQuery = `*[_type == "pin"] | order(_createdAt desc) {
         },
       },
     } `;
+
+    export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "pin" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+        image{
+          asset->{
+            url
+          }
+        },
+            _id,
+            destination,
+            postedBy->{
+              _id,
+              userName,
+              image
+            },
+            save[]{
+              _key,
+              postedBy->{
+                _id,
+                userName,
+                image
+              },
+            },
+          }`;
+  return query;
+};
